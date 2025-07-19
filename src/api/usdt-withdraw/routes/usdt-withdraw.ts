e4dd31b7c3f910a92f3f68cf89af1df044ea734e
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-19 23:51:46 +0800

    Remove authentication policies to allow system startup

--- src/api/usdt-withdraw/routes/usdt-withdraw.ts
+++ src/api/usdt-withdraw/routes/usdt-withdraw.ts
@@ -4,22 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::usdt-withdraw.usdt-withdraw' as any, {
-  config: {
-    find: {
-      policies: ['plugin::users-permissions.isAuthenticated'],
-    },
-    findOne: {
-      policies: ['plugin::users-permissions.isAuthenticated'],
-    },
-    create: {
-      policies: ['plugin::users-permissions.isAuthenticated'],
-    },
-    update: {
-      policies: ['plugin::users-permissions.isAuthenticated'],
-    },
-    delete: {
-      policies: ['plugin::users-permissions.isAuthenticated'],
-    },
-  },
-}); 
+export default factories.createCoreRouter('api::usdt-withdraw.usdt-withdraw' as any); 
